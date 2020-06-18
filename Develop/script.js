1632b519a8f17f1c89077e54b76f62b6ac592625
Mustafa Dag <mustafaa_dag@hotmail.com>
2020-06-18 00:22:50 -0400

    still working on it

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // GIVEN I need a new, secure password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -68,10 +67,6 @@ var specialCharacter = ["!","#","$","%","&","(",")","*","+","-","/",":",";","<",
 // *********************** end function logic **********************
 
 
-
-
-
-
 // Add event listener to generate button
 // WHEN I click the button to generate a password
-// generateBtn.addEventListener("click", writePassword);
+ generateBtn.addEventListener("click", writePassword);
